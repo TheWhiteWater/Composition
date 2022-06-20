959a51a12dd510a6a7fb53d90c2bdf235d07722a
TheWhiteWater <ali.agzamov@gmail.com>
2022-06-20 21:44:36 +1200

    Old style fragment navigation released

--- app/src/main/java/nz/co/redice/composition/domain/entity/GameResult.kt
+++ app/src/main/java/nz/co/redice/composition/domain/entity/GameResult.kt
@@ -1,8 +1,12 @@
 package nz.co.redice.composition.domain.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class GameResult(
     val winner: Boolean,
     val countOfRightAnswers: Int,
     val countOfAnsweredQuestions: Int,
     val gameSettings: GameSettings
-)
+) : Parcelable
